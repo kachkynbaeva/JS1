d0ea94fcf6cda191c04ecb1946cb0c39a7d0aa26
kachkynbaeva <kunduz1604@icloud.com>
2023-10-25 20:10:12 +0600

    added

---
+++ script.js
@@ -0,0 +1,33 @@
+// boolean =true/false
+// string =''/"
+// number =1
+// underfined
+// null =
+// symbol =
+// bigInt =
+
+// let a = 4
+// let b = 55
+// console.log(a + b)
+// let a =55
+// let b =4
+// console.log (a % b)
+// let a = 'hello '
+// let b ='kunduz'
+// console.log(a+b)
+// let a = 'happy'
+// let b = '3'
+// console.log(a-b)
+// let a =5
+// let b ='5'
+// console.log(a + + b)
+// let a =4
+// let b =55
+// console.log(a / b)
+// let a = 4
+// let b =55
+// console.log(a* b)
+let c = 2
+let b = undefined
+console.log(c + b)
+
